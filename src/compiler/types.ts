7119c6f4400531cfbfd2c14322e4710379bfc1d3
Sheetal Nandi <shkamat@microsoft.com>
2023-10-12 14:34:14 -0700

    If typing installer is disabled invalidate all the resolutions from typings cache This change finally makes all tests pass incremental tests for matching resolutions and program structuture

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -7962,6 +7962,8 @@ export interface ResolvedModuleWithFailedLookupLocations {
      * have been resolvable under different module resolution settings.
      */
     alternateResult?: string;
+    /** @internal */
+    globalCacheResolution?: boolean;
 }
 
 export interface ResolvedTypeReferenceDirective {
